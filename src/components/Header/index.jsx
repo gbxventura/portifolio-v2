21d0b1d204909e9972b67b5537a7c70bf2438013
gbxventura <gabriel.ventura20200@gmail.com>
2024-06-12 22:57:42 -0300

    Responsividade do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,10 +3,11 @@ import styles from './Header.module.css';
 import { useState } from 'react';
 
 function Header() {
-    const [showMenu, setShowMenu] = useState(false)
-    const togleMenu = () => {
-        setShowMenu(!showMenu)
-    }
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
 
     return (
         <header className={styles.header}>
@@ -15,22 +16,22 @@ function Header() {
             </Link>
             <nav
                 className={`${styles.menuSandwich} ${showMenu ? styles.show : ''}`}
-                onClick={togleMenu}
             >
-                <Link to='/'>Home</Link>
-                <Link to='/sobre'>Sobre</Link>
-                <Link to='/projetos'>Projetos</Link>
-                <Link to='/contatos'>Contatos</Link>
+                <Link to='/' onClick={toggleMenu}>Home</Link>
+                <Link to='/sobre' onClick={toggleMenu}>Sobre</Link>
+                <Link to='/projetos' onClick={toggleMenu}>Projetos</Link>
+                <Link to='/contatos' onClick={toggleMenu}>Contatos</Link>
             </nav>
             <div
                 className={styles.menuButton}
-                onClick={togleMenu}
+                onClick={toggleMenu}
             >
                 <span className={styles.linha}></span>
                 <span className={styles.linha}></span>
                 <span className={styles.linha}></span>
             </div>
         </header>
-    )
+    );
 }
+
 export default Header;
